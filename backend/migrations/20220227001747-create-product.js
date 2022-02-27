83640e3c6e6d02d2f641a913fe4242b2e692efdd
Thiago Gomes <thiagog.macedom@gmail.com>
2022-02-27 03:35:37 -0300

    feat: database working, need some refactoring but now i will continue developing,, next steps will be implement whole api functions

---
+++ backend/migrations/20220227001747-create-product.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      product_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+      },
+      product_quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  },
+};
---
+++ backend/models/user.js
@@ -0,0 +1,26 @@
+'use strict';
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    user_name: {type: DataTypes.STRING, allowNull: false},
+    user_password: {type: DataTypes.STRING, allowNull: false},
+    user_email: {type: DataTypes.STRING, allowNull: false},
+    user_role: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  });
+
+  User.associate = (models) => {
+    User.belongsTo(models.Role, {
+      foreignKey: 'user_role',
+      as: 'role',
+    });
+  };
+
+  return User;
+};
+
+module.exports = User;
