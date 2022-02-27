daff746e3bde42dd3a7b782e4edcb25ea2cfe761
Thiago Gomes <thiagog.macedom@gmail.com>
2022-02-26 21:37:58 -0300

    fix: refatoring to use .env port

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,5 +1,7 @@
 const app = require('./app');
+require('dotenv').config();
 
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
