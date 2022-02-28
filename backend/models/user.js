0fe5d214792fd31c75681b6c1edb2a8eb5e61328
Thiago Gomes <thiagog.macedom@gmail.com>
2022-02-27 21:09:47 -0300

    fix: small change on user model code

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,8 +2,8 @@
 const User = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     user_name: {type: DataTypes.STRING, allowNull: false},
-    user_password: {type: DataTypes.STRING, allowNull: false},
     user_email: {type: DataTypes.STRING, allowNull: false},
+    user_password: {type: DataTypes.STRING, allowNull: false},
     user_role: {
       type: DataTypes.INTEGER,
       allowNull: false,
