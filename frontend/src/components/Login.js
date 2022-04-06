34bd017490f467175e850b19dd296542d55cc9e8
Thiago Gomes <thiagog.macedom@gmail.com>
2022-04-06 00:41:59 -0300

    feat: create logic to capture user login data

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {LoginContainer, LoginInput, LoginButton} from './Login.style';
 
 function Login() {
+	const [loginInfo, setLoginInfo] = useState({
+		username: '',
+		password: '',
+	});
+
+	const handleChange = e => {
+		setLoginInfo(prevState => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}),
+		);
+	};
+
 	return (
 		<LoginContainer>
 			<LoginInput
 				name="username"
 				placeholder="Insira seu email"
+				value={loginInfo.username}
+				onChange={handleChange}
 			/>
 			<LoginInput
 				name="password"
 				placeholder="Insira sua senha"
 				type="password"
+				value={loginInfo.password}
+				onChange={handleChange}
 			/>
 			<LoginButton
-				onClick={() => console.log('clicou')}
+				onClick={() => console.log('fodas')}
 			>
         Entrar
 			</LoginButton>
